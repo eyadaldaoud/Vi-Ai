f2cbedcbcfd6b4d0a555dc758a8fd9676c08d4a5
UGoingNoWhereBoy <ugnw20@gmail.com>
2023-03-25 08:23:20 -0700

    Added Audiences in Vercel Analytics

--- src/app/Client.jsx
+++ src/app/Client.jsx
@@ -1,11 +1,13 @@
 'use client'
 
 const { ThemeProvider } = require("next-themes")
+import { Analytics } from '@vercel/analytics/react';
 
 const Client = ({ children }) => {
   return (
     <ThemeProvider attribute="class" enableSystem>
         {children}
+        <Analytics />
     </ThemeProvider>
   )
 }
