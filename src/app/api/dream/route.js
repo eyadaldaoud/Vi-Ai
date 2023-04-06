7b7ca3220216c7d8f6318bfdebfd2e782913e2d9
Eyad Zoubi <103299832+UGoingNoWhereBoy@users.noreply.github.com>
2023-04-06 18:57:52 -0400

    Update route.js

--- src/app/api/dream/route.js
+++ src/app/api/dream/route.js
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server';
 import WomboDream from 'dream-api'
 
 export async function POST(request){
-    const resault = await request.json();
-    const { question } = resault;
+    const result = await request.json();
+    const { question } = result;
     const response = await WomboDream.generateImage(1, question);
     const images =  response;
     return NextResponse.json({message: images})
